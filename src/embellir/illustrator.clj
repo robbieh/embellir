92804638b251cc43436e0697dd012a25e1ea0d2b
Robbie Huffman <robbie.huffman@gmail.com>
2012-12-27 12:20:38 -0500

    sys-move works now

--- src/embellir/illustrator.clj
+++ src/embellir/illustrator.clj
@@ -1,6 +1,6 @@
 (ns embellir.illustrator
   (:gen-class)
-  (:require [clj-time]
+  (:require [clj-time.core]
             [clj-time.coerce]
             [clj-time.local])
   (:use [quil.core])
@@ -12,11 +12,23 @@
 
 (def entities "the entities in this component entity system" (atom []))
 
+(defn now-long [] (clj-time.coerce/to-long (clj-time.local/local-now)))
+
 (defn create-component
   [compname & compmap]
   ;TODO: check that compmap is all keywords
   (intern *ns* (symbol compname) 
           (fn [& more] {(keyword compname) (zipmap compmap more)})))
+
+(defn add-component-to-entity 
+  [entityname, component]
+  (swap! entities 
+         #(map (fn merge-if [e] (if (= (:name e) entityname) (merge e component) e)) % )))
+
+(defn remove-component-from-entity
+  [entityname, componentkey]
+  (swap! entities 
+         #(map (fn dissoc-if [e] (if (= (:name e) entityname) (dissoc e componentkey) e)) % )))
 
 (defn create-entity
   "Creates an entity with the specified components"
@@ -50,7 +62,43 @@
       (pop-matrix) (pop-style)
       )))
 
+(defn pctpoint [p1 p2 pct]
+  (if (< p1 p2) (+ p1 (* (abs (- p1 p2)) pct))
+                (- p1 (* (abs (- p1 p2)) pct))))
 
+(defn move-linear [startpt endpt pct]
+  (let [x (pctpoint (:x startpt) (:x endpt) pct)
+        y (pctpoint (:y startpt) (:y endpt) pct) ]
+    {:x x :y y}))
+
+(defn sys-move
+  "moves everything with a moveto component"
+  []
+  (doseq [entity (get-entities :moveto)]
+    ;if there is no :x in :moveto, copy :x and :y from :position
+    (let [entname (:name entity)
+          mcomp (:moveto entity)
+          poscomp (:position entity)
+          now (now-long)
+          starttime (:starttime mcomp)
+          endtime (:endtime mcomp)
+          rate-fn (:rate-fn mcomp)
+          nnow (- now starttime) ;'normalized' now
+          timediff (- endtime starttime)
+          timepct (min 1 (/ nnow timediff)) ;what percentage of time has passed
+          startpt (if (:x mcomp) (select-keys mcomp [:x :y]) ;use x,y if there, if not copy from position
+                    (let [poscomp (:position entity)
+                          entname (:name entity)
+                          mcomp (:moveto entity)
+                          newmcomp (assoc mcomp :x (:x poscomp) :y (:y poscomp))]
+                      (add-component-to-entity entname {:moveto newmcomp})
+                      (select-keys newmcomp [:x :y])))
+          endpt {:x (:endx mcomp)  :y (:endy mcomp)}
+          nowpt (move-linear startpt endpt timepct)
+          newpos (merge poscomp nowpt)]
+      (if (= 1 timepct) (remove-component-from-entity entname :moveto))
+      (add-component-to-entity entname {:position newpos})
+      )))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -59,7 +107,7 @@
 (create-component "position" :x :y :r)
 (create-component "bound" :shape :width :height :more)
 (create-component "drawing" :fn)
-(create-component "moveto" :x :y :starttime :endtime :rate-fn)
+(create-component "moveto" :endx :endy :starttime :endtime :rate-fn)
 (create-component "spin" :endrad :starttime :endtime :rate-fn)
 (create-component "resize" :endwidth :endheight :starttime :endtime :rate-fn)
 
@@ -78,6 +126,7 @@
 (defn draw []
   (background 0)
   (sys-draw)
+  (sys-move)
   )
 
 (defn start-sketch []
