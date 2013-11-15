b960865c6040a68f31aaa4bc1d96147c81131c93
Robbie Huffman <robbie.huffman@gmail.com>
2013-11-15 16:59:28 -0500

    iplist tracks count and first seen time

--- src/embellir/curios/iplist.clj
+++ src/embellir/curios/iplist.clj
@@ -1,5 +1,6 @@
 (ns embellir.curios.iplist
-
+;     [clj-time.core :as clj-time]
+     (:require [clj-time.local]) 
   )
 
 ;{:name "weather" 
@@ -11,28 +12,33 @@
 ; http://graphical.weather.gov/xml/
 ;
 
- 
+(defn update-values  [m f & args]
+   (reduce  (fn  [r  [k v]]  (assoc r k  (apply f v args)))  {} m)) 
 
+(defn get-count [ip m]
+  (get ip m)
+  )
 
 (defn update-iplist
-  [wdata] 
-  wdata
-  )
-(defn receive-iplist
-  [kdata rmap] 
-  (merge kdata rmap)
+  [ipdata] 
+  ipdata
   )
 
-(defn curation-map [] {:atom (atom   {"0.0.0.0" {}
-                                     "8.8.8.8" {}
-                                     "255.255.255.255" {}
-                                     "0.0.255.255" {}
-                                     "255.255.0.0" {}
-                                     "207.69.188.185" {}
-                                      }
-                                   )
+(defn receive-iplist
+  [ipdata rmap] 
+  (let [now (clj-time.local/local-now)
+        dmap (update-values rmap conj {:last-seen now})
+        cmap (into {} (for [[k v] (merge ipdata dmap)] 
+                        (let [x (get-in ipdata [k :count] 0 )]
+                           [k (assoc v :count (inc x))])
+                        ))
+        ]
+    
+    
+    cmap)
+  )
+
+(defn curation-map [] {:atom (atom   {})
                        :function embellir.curios.iplist/update-iplist
                        :time-to-live (* 1000 20) ;every few seconds
                        :receiver-function embellir.curios.iplist/receive-iplist})
-
-
